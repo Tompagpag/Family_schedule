351cdb87681655b39b80ab3a6088bf445dce6e6d
Tom Paganel <paganel.tom@gmail.com>
2021-11-23 16:09:02 +0100

    partial

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,25 @@
 class EventsController < ApplicationController
+  extend SimpleCalendar
+
   def new
+    @event = Event.new
+  end
+
+  def index
+    @events = Event.all
   end
 
   def create
+    binding.pry
+    @event = Event.new(event_params)
+    @family = Family.find(params[:family_id])
+    @event.family_id = @family.id
+    @event.user = current_user
+    if @event.save
+      redirect_to family_conflicts
+    else
+      render :new
+    end
   end
 
   def edit
@@ -12,5 +29,11 @@ class EventsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :start_date, :end_date, :comment)
   end
 end
