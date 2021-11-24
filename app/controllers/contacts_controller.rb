57aaaf4d11d7ba070e954adb30aa141e047c4d55
Benjamin McConnell <crosslegged@gmail.com>
2021-11-24 12:48:01 +0100

    merging commit

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,11 +4,27 @@ class ContactsController < ApplicationController
   end
 
   def new
+    @contact = Contact.new
+    @family = Family.find(params[:family_id])
   end
 
   def create
+    @contact = Contact.new(contact_params)
+    @family = Family.find(params[:family_id])
+    @contact.family = @family
+    if @contact.save
+      redirect_to contacts_path
+    else
+      render :new
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:first_name, :last_name, :email, :phone_number, :comment)
   end
 end
