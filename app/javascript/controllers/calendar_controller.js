5033adb6b16a36da6e243b9aed6a1eff0edf68f0
Emmanuelle du Colombier <emmanuelle.ducolombier@gmail.com>
2021-11-26 15:14:10 +0100

    change conflict types

--- app/javascript/controllers/calendar_controller.js
+++ app/javascript/controllers/calendar_controller.js
@@ -7,12 +7,12 @@ export default class extends Controller {
   connect() {
 
     this.conflictTargets.forEach((conflict) => {
-      if (conflict.classList.contains('babysitter')) {
+      if (conflict.classList.contains('transport')) {
         if (conflict.closest('.test').getElementsByClassName('fa-car').length === 0) {
           conflict.closest('.test').insertAdjacentHTML('beforeend', '<i class="fas fa-car"></i>');
         };
       };
-      if (conflict.classList.contains('transport')) {
+      if (conflict.classList.contains('babysitter')) {
         if (conflict.closest('.test').getElementsByClassName('fa-child').length === 0) {
           conflict.closest('.test').insertAdjacentHTML('beforeend', '<i class="fas fa-child"></i>');
         };
