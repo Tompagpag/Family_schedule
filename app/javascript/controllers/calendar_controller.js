5a3c01c938fe6a7b6e6810953c1f6b3a5a9da00b
Emmanuelle du Colombier <emmanuelle.ducolombier@gmail.com>
2021-11-26 14:41:36 +0100

    style

--- app/javascript/controllers/calendar_controller.js
+++ app/javascript/controllers/calendar_controller.js
@@ -1,3 +1,4 @@
+import { end } from "@popperjs/core";
 import { Controller } from "stimulus";
 
 export default class extends Controller {
@@ -6,21 +7,17 @@ export default class extends Controller {
   connect() {
 
     this.conflictTargets.forEach((conflict) => {
-      if (conflict.classList.contains('conflict')) {
-        conflict.closest('.day').classList.add("parent-conflict-bg");
-        // conflict.closest('.day').classList.remove("has-events");
-        // conflict.closest('.test').classList.add("test-circle");
-      }
-    })
-  }
+      if (conflict.classList.contains('babysitter')) {
+        if (conflict.closest('.test').getElementsByClassName('fa-car').length === 0) {
+          conflict.closest('.test').insertAdjacentHTML('beforeend', '<i class="fas fa-car"></i>');
+        };
+      };
+      if (conflict.classList.contains('transport')) {
+        if (conflict.closest('.test').getElementsByClassName('fa-child').length === 0) {
+          conflict.closest('.test').insertAdjacentHTML('beforeend', '<i class="fas fa-child"></i>');
+        };
+      };
+    });
+  };
 
-  display(evt) {
-    this.displayTarget.classList.toggle("d-none");
-   // evt.currentTarget.querySelector('.details').cl
-  }
-
-  // circle(evt) {
-  //   console.log(evt);
-  //   this.displayTarget.classList.add("day-number");
-  // }
 }
