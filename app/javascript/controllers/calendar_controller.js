f60d63dc123b789292e5da7872fe132b186b6144
Emmanuelle du Colombier <emmanuelle.ducolombier@gmail.com>
2021-11-28 18:33:17 +0100

    encore des modifs

--- app/javascript/controllers/calendar_controller.js
+++ app/javascript/controllers/calendar_controller.js
@@ -5,7 +5,6 @@ export default class extends Controller {
   static targets = ["days", "updateConflict", "conflict"];
 
   connect() {
-
     this.conflictTargets.forEach((conflict) => {
       if (conflict.closest('.test').getElementsByClassName('fa-car').length === 0) {
         conflict.closest('.test .eventattention').insertAdjacentHTML('beforeend', '<img class="attention" src="https://img.icons8.com/material-outlined/50/fa314a/box-important.png"/>');
@@ -13,9 +12,12 @@ export default class extends Controller {
     });
   };
 
-  display(event) {
-    console.log(this.updateConflictTargets)
-    this.updateConflictTargets.classList.remove("d-none")
+  display() {
+    this.updateConflictTargets.forEach((div) => {
+      if (div.closest('user-event').attr('class').contains("true")) {
+        div.classList.remove("d-none")
+      }
+    })
   }
 
   // this.daysTargets.forEach((calendarDay) => {
