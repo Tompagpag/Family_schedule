d58c49c22855c74dac92b084ecb61d36c9f93878
Stregold <stregold@gmail.com>
2021-11-29 11:01:04 +0100

    column and css done

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,5 +2,5 @@ class Contact < ApplicationRecord
   belongs_to :family
   has_many :conflicts
 
-  validates :first_name, :last_name, :email, :phone_number, presence: true
+  validates :first_name, :last_name, :email, :phone_number, :comment, presence: true
 end
