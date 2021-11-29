ef0e89e63d710aa8916531560555ecda67b1d96b
Emmanuelle du Colombier <emmanuelle.ducolombier@gmail.com>
2021-11-29 16:22:12 +0100

    commit

--- app/controllers/conflicts_controller.rb
+++ app/controllers/conflicts_controller.rb
@@ -5,12 +5,6 @@ class ConflictsController < ApplicationController
     @events = Event.all
   end
 
-  def show
-    @conflict = Conflict.find(params[:id])
-    # @events = Event.where("start_at LIKE ?", @conflict.events.first.start_at.strftime("%y-%m-%d"))
-    # @events = Event.where("date(start_at) = ?", @conflict.date)
-  end
-
   def create
   end
 
